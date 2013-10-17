4e42941c5939cd99a2a3b81ffada7384726d750f
Simón Moya <simonmoya@gmail.com>
2013-10-17 13:42:14 -0500

    Inclusión correo hmoreno@ospinternational.com

--- www/js/base_datos.js
+++ www/js/base_datos.js
@@ -106,11 +106,12 @@ tx.executeSql("Delete from parametro;")
 
 tx.executeSql("insert into parametro values(1,'simonmoya@gmail.com','E-mail Ministerio de Justicia',1);")
 tx.executeSql("insert into parametro values(2,'winnie54817@gmail.com','E-mail Ministerio de Justicia',1);")
-tx.executeSql("insert into parametro values(3,'MinjusticiaCo','Ministerio de Justicia',2);")
-tx.executeSql("insert into parametro values(4,'hC9tD_GTxNg','Casas de Justicia',3);")
-tx.executeSql("insert into parametro values(5,'mcf8Wwqw4JQ','Centros de Convivencia Ciudadana',3);")
-tx.executeSql("insert into parametro values(6,'http://servicedatosabiertoscolombia.cloudapp.net/v1/Ministerio_de_Justicia/informacionprogramas','Información de Programas',4);")
-tx.executeSql("insert into parametro values(7,'http://servicedatosabiertoscolombia.cloudapp.net/v1/Ministerio_de_Justicia/ubicacionprogramas','Ubicación de Programas',4);")
+tx.executeSql("insert into parametro values(3,'hmoreno@ospinternational.com','E-mail Ministerio de Justicia',1);")
+tx.executeSql("insert into parametro values(4,'MinjusticiaCo','Ministerio de Justicia',2);")
+tx.executeSql("insert into parametro values(5,'hC9tD_GTxNg','Casas de Justicia',3);")
+tx.executeSql("insert into parametro values(6,'mcf8Wwqw4JQ','Centros de Convivencia Ciudadana',3);")
+tx.executeSql("insert into parametro values(7,'http://servicedatosabiertoscolombia.cloudapp.net/v1/Ministerio_de_Justicia/informacionprogramas','Información de Programas',4);")
+tx.executeSql("insert into parametro values(8,'http://servicedatosabiertoscolombia.cloudapp.net/v1/Ministerio_de_Justicia/ubicacionprogramas','Ubicación de Programas',4);")
 
             });
 /* ---------------------------------------------------------------------- */
